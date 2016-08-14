fbfdf02c4bc4f91dd34e89fdbc79dace0b64a46e
Vicente <masip85@gmail.com>
2016-08-14 13:15:31 +0200

    test app

---
+++ test/test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by ladmin on 13/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ test/testTests/testTests.h
@@ -0,0 +1,13 @@
+//
+//  testTests.h
+//  testTests
+//
+//  Created by ladmin on 13/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testTests : SenTestCase
+
+@end
